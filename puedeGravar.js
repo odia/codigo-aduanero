199c40ae1193792356ac901f125062a9e06f29a7
Guido Vilariño <gvilarino@gmail.com>
2019-05-13 19:34:29 -0300

    1981 - Código Aduanero (Decreto Ley)

--- puedeGravar.js
+++ puedeGravar.js
@@ -1,5 +1,20 @@
 function puedeGravar(autoridad, impuesto) {
 
+  // Código Aduanero 1981 - Art. 755 (Decreto Ley)
+  if (autoridad.esPoderEjecutivo() && impuesto.esAduanero()) {
+
+    return maximizaValorAgregado(impuesto) || faltaGuita();
+  }
+
   // CN 1853 - Art. 75
   return autoridad.esPoderLegislativo();
+}
+
+function maximizaValorAgregado(impuesto) {
+  // un montón de condiciones oscuras
+}
+
+function faltaGuita() {
+  // Demasiado nunca es suficiente
+  return poderEjecutivo.faltaGuita();
 }
